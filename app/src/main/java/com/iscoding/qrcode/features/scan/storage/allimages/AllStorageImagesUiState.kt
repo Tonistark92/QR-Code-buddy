11641ebc78b77bf259ad00e922f8327cd43ea4d9
Islam_Dev <hanyislam270@gmail.com>
2025-09-23 03:28:55 +0300

    [1;33m📝 Enter your commit message:[0m test scripts 2

--- app/src/main/java/com/iscoding/qrcode/features/scan/storage/allimages/AllStorageImagesUiState.kt
+++ app/src/main/java/com/iscoding/qrcode/features/scan/storage/allimages/AllStorageImagesUiState.kt
@@ -1,12 +1,12 @@
 package com.iscoding.qrcode.features.scan.storage.allimages
 
 
-data class AllStorageImagesUiState(
-    val hasCamPermission: Boolean = false,
-    val shouldPermissionDialog: Boolean = false,
-    val shouldURLDialog: Boolean = false,
-    val shouldLaunchAppSettings: Boolean = false,
-    val scannedData: String = "",
-    val scannedUrl: String = "",
-    val isGoodUrlRegex: Boolean = false,
-)
+//data class AllStorageImagesUiState(
+//    val hasCamPermission: Boolean = false,
+//    val shouldPermissionDialog: Boolean = false,
+//    val shouldURLDialog: Boolean = false,
+//    val shouldLaunchAppSettings: Boolean = false,
+//    val scannedData: String = "",
+//    val scannedUrl: String = "",
+//    val isGoodUrlRegex: Boolean = false,
+//)
